699beed7b1696bc43e92847ede47257756eae61d
Stefan <39183620+Areskiko@users.noreply.github.com>
2020-10-31 14:27:33 +0100

    Added event listing

--- src/eventParser.py
+++ src/eventParser.py
@@ -2,7 +2,18 @@ import requests
 import json
 import datetime
 import re
+from bs4 import BeautifulSoup
 
+
+def listEvents():
+    url = "https://abakus.no/events"
+    listPage = requests.get(url).content
+    soup = BeautifulSoup(listPage, "html.parser")
+    events = []
+    for eventCategory in soup.findAll("div", {"class": "EventList__eventGroup--1-Btpkldi0"}):
+        anchors = eventCategory.findAll("a")
+        events.extend([x["href"][-4:] for x in anchors])
+    return events
 
 
 def getEvent(eventId):
@@ -13,9 +24,12 @@ def getEvent(eventId):
     description = data["description"]
     eventTime = data["startTime"]
     eventLocation = data["location"]
-    rawRegistrationOpen = data["pools"][0]["activationDate"]
-    regex = re.search("(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z", rawRegistrationOpen)
-    registrationOpen = datetime.datetime(int(regex.group(1)), int(regex.group(2)), int(regex.group(3)), int(regex.group(4)), int(regex.group(5)), int(regex.group(6)))
+    try:
+        rawRegistrationOpen = data["pools"][0]["activationDate"]
+        regex = re.search("(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2})Z", rawRegistrationOpen)
+        registrationOpen = datetime.datetime(int(regex.group(1)), int(regex.group(2)), int(regex.group(3)), int(regex.group(4)), int(regex.group(5)), int(regex.group(6)))
+    except IndexError:
+        registrationOpen = None
     return {
         "name": name,
         "description": description,
@@ -25,4 +39,3 @@ def getEvent(eventId):
     }
 
 
-print(getEvent(2773))
