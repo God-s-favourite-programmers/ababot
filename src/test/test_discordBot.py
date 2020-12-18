8c7f5f4120cf50d13e0ed00f036a01bf6e75fb25
Areskiko <39183620+Areskiko@users.noreply.github.com>
2020-12-18 13:30:00 +0100

    Added timeout for pytest (#10)
    * Added timeout for pytest
    
    * Changed to custom test to facilitate token as argument

--- src/test/test_discordBot.py
+++ src/test/test_discordBot.py
@@ -1,12 +1,24 @@
-import os
-import time
+import asyncio
+import sys, os
 
 def testBot():
-    import sys, os
-    myPath = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, myPath + '/../')
-    import discordBot
-    token = os.environ["TOKEN"]
-    discordBot.client.run(token)
-    time.sleep(5)
-    discordBot.client.logout()
+    try:
+        myPath = os.path.dirname(os.path.abspath(__file__))
+        sys.path.insert(0, myPath + '/../')
+        from discordBot import client
+        args = sys.argv
+        token = args[1]
+        
+        
+        async def go():
+            try:
+                await asyncio.wait_for(client.start(token), timeout=5)
+            except asyncio.TimeoutError:
+                await client.close()
+
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(go())
+        return 0
+    except:
+        return 1
+testBot()
