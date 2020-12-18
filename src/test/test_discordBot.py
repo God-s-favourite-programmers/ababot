c6da3eec935af86e5c8cff123002741b5cd717d4
Areskiko <39183620+Areskiko@users.noreply.github.com>
2020-12-18 13:28:36 +0100

    Changed to custom test to facilitate token as argument

--- src/test/test_discordBot.py
+++ src/test/test_discordBot.py
@@ -2,17 +2,23 @@ import asyncio
 import sys, os
 
 def testBot():
-    myPath = os.path.dirname(os.path.abspath(__file__))
-    sys.path.insert(0, myPath + '/../')
-    from discordBot import client
-    token = os.environ["TOKEN"]
-    
-    
-    async def go():
-        try:
-            await asyncio.wait_for(client.start(token), timeout=5)
-        except asyncio.TimeoutError:
-            await client.close()
+    try:
+        myPath = os.path.dirname(os.path.abspath(__file__))
+        sys.path.insert(0, myPath + '/../')
+        from discordBot import client
+        args = sys.argv
+        token = args[1]
+        
+        
+        async def go():
+            try:
+                await asyncio.wait_for(client.start(token), timeout=5)
+            except asyncio.TimeoutError:
+                await client.close()
 
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(go())
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(go())
+        return 0
+    except:
+        return 1
+testBot()
