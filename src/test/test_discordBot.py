9d0e3a8972aced5e9859dee2e313abd19144382f
Areskiko <39183620+Areskiko@users.noreply.github.com>
2020-12-18 11:40:36 +0100

    Added testing

--- src/test/test_discordBot.py
+++ src/test/test_discordBot.py
@@ -2,6 +2,9 @@ import os
 import time
 
 def testBot():
+    import sys, os
+    myPath = os.path.dirname(os.path.abspath(__file__))
+    sys.path.insert(0, myPath + '/../')
     import discordBot
     token = os.environ["TOKEN"]
     discordBot.client.run(token)
