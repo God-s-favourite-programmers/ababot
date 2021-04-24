5971f6f277a4f9f7e46645156e714f1bb8e5b39f
Stefan <stefan_mack@hotmail.com>
2021-04-24 17:58:04 +0200

    Added cogs reload command

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from json import load
 import logging
 logging.basicConfig(
     filename='/ababot/ababot.log',
@@ -21,10 +22,29 @@ async def on_ready():
     print("AbaBot is ready")
     logger.info("Bot started")
 
-if __name__ == "__main__":
+@client.command()
+async def reload(ctx):
+    logger.info("Reloading all cogs")
+    async with ctx.typing():
+        unload_all_cogs()
+        load_all_cogs()
+    await ctx.send("Reload complete")
+
+def load_all_cogs():
     logger.info("Loading cogs")
     cogs = [f.name for f in os.scandir("./src/cogs")]
     for cog in cogs:
         client.load_extension(f"src.cogs.{cog}.{cog}")
+
+def unload_all_cogs():
+    logger.info("Unloading cogs")
+    cogs = [f.name for f in os.scandir("./src/cogs")]
+    for cog in cogs:
+        client.unload_extension(f"src.cogs.{cog}.{cog}")
+
+
+
+if __name__ == "__main__":
+    load_all_cogs()
     logger.info("Running client")
     client.run(getToken.token)
