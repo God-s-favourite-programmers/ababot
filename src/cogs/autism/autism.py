0061591767c275ac4f15b8caad99bad9c560d3f3
Henrik <henriktl@stud.ntnu.no>
2021-05-16 20:33:28 +0200

    It wil move members

--- src/cogs/autism/autism.py
+++ src/cogs/autism/autism.py
@@ -1,6 +1,8 @@
 import logging
 import discord
+from discord.errors import HTTPException
 from discord.ext import commands
+from discord.ext.commands.errors import CommandInvokeError
 
 logger = logging.getLogger(__name__)
 
@@ -14,9 +16,6 @@ class Autism(commands.Cog):
         self.client = client
         self.name = type(self).__name__
         self.guild = self.client.guilds[0]
-        self.channelId = discord.utils.get(
-            self.client.get_all_channels(), guild=self.guild, name='Helpdesk').id
-        self.channel = self.client.get_channel(self.channelId)
         print(f"Cog {self.name} loaded")
         logger.info(f"Cog {self.name} loaded")
 
@@ -30,7 +29,7 @@ class Autism(commands.Cog):
     async def ping(self, ctx):
         Responds with Pong!
         await ctx.send("Pong!")"""
-j
+
     """@ping.error
     async def ping_error(self, ctx, error):
         Handles error originating from ping command
@@ -41,13 +40,18 @@ j
     @commands.command()
 
     async def em(self, ctx, name="Lounge"):
-        async for mem in self.guild.fetch_members(limit=150):
-            print(mem)
-            if mem.voice != None:
-                await mem.move_to(self.channel)
+        try:
+            self.channelId = discord.utils.get(
+                self.client.get_all_channels(), guild=self.guild, name=name).id
+            self.channel = self.client.get_channel(self.channelId)
+            async for mem in self.guild.fetch_members(limit=150):
+                print(mem)
+                if mem.voice != None:
+                    await mem.move_to(self.channel)
+        except (CommandInvokeError, AttributeError,HTTPException):   
+            await ctx.send("lease specify channel correctly")
 
 
 def setup(client):
     """Sets up the cog."""
-
     client.add_cog(Autism(client))
