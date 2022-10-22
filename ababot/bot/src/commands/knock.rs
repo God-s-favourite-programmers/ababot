14dcb6747ae18330a69a9e4be9cb1f1b0a032edb
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-10-22 17:53:00 +0200

    Add knock knock

---
+++ ababot/bot/src/commands/knock.rs
@@ -0,0 +1,9 @@
+use serenity::{model::prelude::interaction::application_command::CommandDataOption, builder::CreateApplicationCommand};
+
+pub fn run(_options: &[CommandDataOption]) -> String {
+    "Come in".to_string()
+}
+
+pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
+    command.name("knock").description("Knock knock")
+}
