8e54c959011eaf443da8ce85f72c3c5d89988597
Areskiko <39183620+Areskiko@users.noreply.github.com>
2022-10-22 22:48:59 +0200

    Remove cfg

--- ababot/bot/src/commands/dice.rs
+++ ababot/bot/src/commands/dice.rs
@@ -6,55 +6,56 @@ use serenity::{
     },
 };
 
-#[cfg(feature = "dice")]
 use rand;
 
 pub fn run(options: &[CommandDataOption]) -> String {
-    #[cfg(feature = "dice")]
-    {
-        let min;
-        let max;
+    let mut min = 0;
+    let mut max = 100;
 
-        for option in options {
-            if option.name == "min" {
-                min = option.value.map(|v| v.as_i64()).flatten().unwrap_or(0);
-            }
-            if option.name == "max" {
-                max = option.value.map(|v| v.as_i64()).flatten().unwrap_or(100);
-            }
+    for option in options {
+        if option.name == "min" {
+            min = option
+                .value
+                .as_ref()
+                .map(|v| v.as_i64())
+                .flatten()
+                .unwrap_or(0);
         }
-        let mut rng = rand::thread_rng();
-        rng.gen_range(min..max).to_string()
+        if option.name == "max" {
+            max = option
+                .value
+                .as_ref()
+                .map(|v| v.as_i64())
+                .flatten()
+                .unwrap_or(100);
+        }
     }
-    #[cfg(not(feature = "dice"))]
-    {
-        "Command is turned off".to_string()
-    }
+    let mut rng = rand::thread_rng();
+    rng.gen_range(min..max).to_string()
 }
 
 pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicationCommand {
-    #[cfg(feature = "dice")]
-    {
-        command
-            .name("Dice")
-            .description("Get a random number")
-            .create_option(|option| {
-                option
-                    .name("min")
-                    .description("The minimum value for the number generator")
-                    .kind(CommandOptionType::Integer)
-                    .min_int_value(0)
-                    .max_int_value(99)
-                    .required(false)
-            })
-            .create_option(|option| {
-                option
-                    .name("max")
-                    .description("The maximum value for the number generator")
-                    .kind(CommandOptionType::Integer)
-                    .min_int_value(1)
-                    .max_int_value(100)
-                    .required(false)
-            })
-    }
+    let c = command
+        .name("Dice")
+        .description("Get a random number")
+        .create_option(|option| {
+            option
+                .name("min")
+                .description("The minimum value for the number generator")
+                .kind(CommandOptionType::Integer)
+                .min_int_value(0)
+                .max_int_value(99)
+                .required(false)
+        })
+        .create_option(|option| {
+            option
+                .name("max")
+                .description("The maximum value for the number generator")
+                .kind(CommandOptionType::Integer)
+                .min_int_value(1)
+                .max_int_value(100)
+                .required(false)
+        });
+    tracing::debug!("Registered Dice command");
+    c
 }
