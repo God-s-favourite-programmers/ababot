15d9792ab010e93358b9d01b58fe38932203df48
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2022-11-02 14:50:47 +0100

    todos made

--- ababot/bot/src/background_tasks/abakus.rs
+++ ababot/bot/src/background_tasks/abakus.rs
@@ -10,12 +10,19 @@ pub async fn run(ctx: Arc<Context>) {
 
     loop {
         let now = chrono::Local::now();
-        let mut target = chrono::Local::today().and_hms(9, 0, 1);
+        let mut target = chrono::Local::today().and_hms(8, 55, 1);
         if now > target {
             target = target + chrono::Duration::days(1);
         }
         let duration = (target - now).to_std().unwrap();
+        // This is early wakeup to begin fetching
         sleep(duration).await;
+
+        // TODO: fetch
+
+        //TODO: premake message
+
+        //TODO: Sleep til 9:00
 
         // TODO fix message to be from abakus
         let channel_message = ChannelId(772092284153757719)
