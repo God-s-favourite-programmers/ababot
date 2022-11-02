b8233ab6cf63aeeb86223f2a4db37ddba64b638e
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2022-11-02 22:35:04 +0100

    Minor speed improvement

--- ababot/bot/src/background_tasks/abakus.rs
+++ ababot/bot/src/background_tasks/abakus.rs
@@ -28,15 +28,19 @@ struct Event {
 impl From<ApiEvent> for Event {
     fn from(api_event: ApiEvent) -> Self {
         Event {
-            title: api_event.title.unwrap_or("".to_string()),
-            description: api_event.description.unwrap_or("".to_string()),
+            title: api_event.title.unwrap_or_else(|| "No title".to_string()),
+            description: api_event
+                .description
+                .unwrap_or_else(|| "No description".to_string()),
             event_time: api_event
                 .event_time
-                .unwrap_or("".to_string())
+                .unwrap_or_else(|| "".to_string())
                 .parse()
-                .unwrap_or(Utc::now()),
-            event_location: api_event.event_location.unwrap_or("N/A".to_string()),
-            thumbnail: api_event.thumbnail.unwrap_or("N/A".to_string()),
+                .unwrap_or_else(|_| Utc::now()),
+            event_location: api_event
+                .event_location
+                .unwrap_or_else(|| "N/A".to_string()),
+            thumbnail: api_event.thumbnail.unwrap_or_else(|| "N/A".to_string()),
         }
     }
 }
