4d005cea2b3d4551fc8d31caff778df7a853a4c2
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2022-11-17 21:55:46 +0100

    Cleanup
    Co-authored-by: Areskiko <Areskiko@users.noreply.github.com>

--- ababot/bot/src/utils/gpgpu/gpu.rs
+++ ababot/bot/src/utils/gpgpu/gpu.rs
@@ -11,13 +11,11 @@ where
     T: GpuWorkType,
 {
     let fw = Framework::default();
-    println!("Running gpu task");
     loop {
         let worker = match receiver.recv() {
             Ok(w) => w,
             Err(_e) => continue,
         };
-        println!("Received work");
 
         let shader = Shader::from_wgsl_file(&fw, &worker.file_name)?;
 
@@ -30,7 +28,7 @@ where
             work_size: worker.work_size,
         };
 
-        let _ = receiver.send(res_worker).unwrap();
+        let _ = receiver.send(res_worker);
     }
 }
 
@@ -46,8 +44,6 @@ where
         gpu_in_buffs.push(noe);
     }
 
-    let gpu_out_buff = GpuBuffer::<T>::with_capacity(&fw, worker.out_data.len() as u64);
-
     // Build descriptor set base
     let mut descriptor_set = DescriptorSet::default();
 
@@ -55,7 +51,8 @@ where
     for buff in gpu_in_buffs.iter() {
         descriptor_set = descriptor_set.bind_buffer(buff, GpuBufferUsage::ReadOnly);
     }
-    // Bind output buffer
+    // Creating and binding output buffer
+    let gpu_out_buff = GpuBuffer::<T>::with_capacity(&fw, worker.out_data.len() as u64);
     descriptor_set = descriptor_set.bind_buffer(&gpu_out_buff, GpuBufferUsage::ReadWrite);
 
     let kernel = Program::new(&shader, "main").add_descriptor_set(descriptor_set);
@@ -83,7 +80,6 @@ mod tests {
         let cpu_data = (0..100).into_iter().collect::<Vec<u32>>();
         let result: Vec<u32> = (0..100).into_iter().map(|_x: u32| 0).collect();
 
-        println!("len of vec: {}", result.len());
 
         let thread_group = Vec3::default();
 
@@ -96,14 +92,9 @@ mod tests {
 
         let cpu_computed_data = (0..10000).into_iter().map(|x| x * 2).collect::<Vec<u32>>();
 
-        // sender.send(worker).await?;
 
         let (left, right) = bichannel::channel::<Worker<u32>, Worker<u32>>();
 
-
-        // let res = gpu_task_2(worker).await?;
-        println!("Starting gpu task");
-        // send right to gpu_task as another thread
         std::thread::spawn(move || {
             gpu_task(right).unwrap();
         });
@@ -112,13 +103,10 @@ mod tests {
         println!("Waiting for gpu task to finish");
 
         let res = left.recv().unwrap().out_data;
-        // panic!("test");
 
         for (a, b) in cpu_computed_data.into_iter().zip(res) {
-            // println!("{} {}", a, b);
             assert_eq!(a, b);
         }
-        println!("Test passed");
 
         Ok(())
     }
