645e56f1f1dd550a2effa439e3c2165cf136d635
Henrik <henrik@stud.ntnu.no>
2022-12-30 22:12:22 +0100

    Some progress. yeah...

--- ababot/bot/src/commands/remindme.rs
+++ ababot/bot/src/commands/remindme.rs
@@ -1,3 +1,4 @@
+use dateparser::parse;
 use serenity::{
     builder::CreateApplicationCommand,
     model::prelude::{
@@ -10,34 +11,63 @@ use serenity::{
 };
 
 pub async fn run(ctx: &Context, command: &ApplicationCommandInteraction) {
-    let mut min = 0;
+    let mut time = chrono::Duration::zero();
+
     for option in &command.data.options {
-        if option.name == "group" {
-            for subopt in &option.options {
-                if subopt.name == "intger" {
-                    for subsubopt in &subopt.options {
-                        if subsubopt.name == "times" {
-                            min = subsubopt
-                                .value
-                                .as_ref()
-                                .and_then(|v| v.as_i64())
-                                .unwrap_or(0);
-                        }
+        if option.name == "form" {
+            for sub_option in &option.options {
+                match sub_option.name.as_str() {
+                    "day" => {
+                        time = time
+                            + chrono::Duration::days(
+                                sub_option.value.as_ref().unwrap().as_i64().unwrap_or(0),
+                            );
+                    }
+                    "hour" => {
+                        time = time
+                            + chrono::Duration::hours(
+                                sub_option.value.as_ref().unwrap().as_i64().unwrap_or(0),
+                            );
+                    }
+                    "minute" => {
+                        time = time
+                            + chrono::Duration::minutes(
+                                sub_option.value.as_ref().unwrap().as_i64().unwrap_or(0),
+                            );
+                    }
+                    _ => {
+                        panic!("Unknown sub option")
                     }
                 }
             }
+        } else if option.name == "string" {
+            for sub_option in &option.options {
+                match sub_option.name.as_str() {
+                    "time" => {
+                        let parsed_time =
+                            parse(sub_option.value.as_ref().unwrap().as_str().unwrap_or("0"));
+                        if parsed_time.is_ok() {
+                            time = parsed_time.unwrap().date_naive().signed_duration_since(
+                                chrono::Utc::now().naive_utc().date(),
+                            );
+                        } else {
+                            panic!("Could not parse time");
+                        }
+                    }
+                    _ => {
+                        panic!("Unknown sub option")
+                    }
+                }
+            }
+        } else {
+            panic!("Unknown option");
         }
     }
     command
         .create_interaction_response(&ctx.http, |response| {
             response
                 .kind(InteractionResponseType::ChannelMessageWithSource)
-                .interaction_response_data(|message| {
-                    message.content(format!(
-                        "This command is not yet implemented but here is the value you gave me: {}",
-                        min
-                    ))
-                })
+                .interaction_response_data(|message| message.content("Comming soon!"))
         })
         .await
         .unwrap();
@@ -50,59 +80,47 @@ pub fn register(command: &mut CreateApplicationCommand) -> &mut CreateApplicatio
         .description("Reminds you of something")
         .create_option(|option| {
             option
-                .name("group")
-                .description("The group to remind you of")
-                .kind(CommandOptionType::SubCommandGroup)
-                .create_sub_option(|subopt| {
-                    subopt
-                        .name("intger")
-                        .description("Reminds you of an integer")
-                        .kind(CommandOptionType::SubCommand)
-                        .create_sub_option(|opt| {
-                            opt.name("times")
-                                .description("The time to remind you of")
-                                .kind(CommandOptionType::Integer)
-                                .required(true)
-                        })
+                .name("form")
+                .description("Just fill in the fields")
+                .kind(CommandOptionType::SubCommand)
+                .create_sub_option(|opt| {
+                    opt.name("message")
+                        .description("The message to remind you of")
+                        .kind(CommandOptionType::String)
+                        .required(true)
                 })
-            // .create_sub_option(|subopt| {
-            //     subopt
-            //         .name("string")
-            //         .description("Reminds you of a string")
-            //         .kind(CommandOptionType::SubCommand)
-            //         .create_sub_option(|opt| {
-            //             opt.name("time")
-            //                 .description("The time to remind you of")
-            //                 .kind(CommandOptionType::String)
-            //         })
-            // })
+                .create_sub_option(|opt| {
+                    opt.name("day")
+                        .description("Days from now")
+                        .kind(CommandOptionType::Integer)
+                })
+                .create_sub_option(|opt| {
+                    opt.name("hour")
+                        .description("Hours from now")
+                        .kind(CommandOptionType::Integer)
+                })
+                .create_sub_option(|opt| {
+                    opt.name("minute")
+                        .description("Minutes from now")
+                        .kind(CommandOptionType::Integer)
+                })
         })
         .create_option(|option| {
             option
-                .name("message")
-                .description("The message to remind you of")
-                .kind(CommandOptionType::SubCommandGroup)
-                .create_sub_option(|subopt| {
-                    subopt
-                        .name("string")
-                        .description("Reminds you of a string")
-                        .kind(CommandOptionType::SubCommand)
-                        .create_sub_option(|opt| {
-                            opt.name("message")
-                                .description("The message to remind you of")
-                                .kind(CommandOptionType::String)
-                        })
+                .name("string")
+                .description("Parse time from string")
+                .kind(CommandOptionType::SubCommand)
+                .create_sub_option(|opt| {
+                    opt.name("message")
+                        .description("The message to remind you of")
+                        .kind(CommandOptionType::String)
+                        .required(true)
                 })
-            // .create_sub_option(|subopt| {
-            //     subopt
-            //         .name("integer")
-            //         .description("Reminds you of an integer")
-            //         .kind(CommandOptionType::SubCommand)
-            //         .create_sub_option(|opt| {
-            //             opt.name("message")
-            //                 .description("The message to remind you of")
-            //                 .kind(CommandOptionType::Integer)
-            //         })
-            // })
+                .create_sub_option(|opt| {
+                    opt.name("time")
+                        .description("The time to remind you of")
+                        .kind(CommandOptionType::String)
+                        .required(true)
+                })
         })
 }
