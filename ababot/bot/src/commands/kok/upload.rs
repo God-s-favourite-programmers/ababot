7c91579c4cf2a2f0015cde1d54f4724e7f07a68a
Henrik Lovhaug <henrik.lovhaug@gmail.com>
2023-01-05 21:02:40 +0100

    Added suppekjøkken

--- ababot/bot/src/commands/kok/upload.rs
+++ ababot/bot/src/commands/kok/upload.rs
@@ -1,14 +1,23 @@
+use std::env;
+
 use serenity::{
-    model::prelude::{
-        component::{ActionRowComponent, InputTextStyle},
-        interaction::{
-            application_command::ApplicationCommandInteraction, modal::ModalSubmitInteraction,
-            InteractionResponseType,
+    futures::StreamExt,
+    model::{
+        prelude::{
+            component::{ActionRowComponent, InputTextStyle},
+            interaction::{
+                application_command::ApplicationCommandInteraction, modal::ModalSubmitInteraction,
+                InteractionResponseType,
+            },
+            GuildId,
         },
+        user::User,
     },
     prelude::Context,
 };
 use tokio::{fs::File, io::AsyncWriteExt};
+
+use crate::utils::get_channel_id;
 
 pub async fn save_small(
     ctx: &Context,
@@ -50,6 +59,7 @@ pub async fn save_small(
 
     // Save file
     file.sync_all().await.unwrap();
+    update_kok_catalogue(ctx, name.to_string(), &command.user).await;
 }
 
 pub async fn save_big(ctx: &Context, command: &ModalSubmitInteraction) {
@@ -140,6 +150,8 @@ pub async fn save_big(ctx: &Context, command: &ModalSubmitInteraction) {
         tracing::warn!("Not able to ack Modal: {:?}", why);
     }
     file.sync_all().await.unwrap(); // Very unliklely to fail at this point. It's to ensure file is actually saved.
+
+    update_kok_catalogue(ctx, name.to_string(), &command.user).await;
 }
 
 async fn error(ctx: &Context, command: &ModalSubmitInteraction) {
@@ -161,6 +173,82 @@ pub fn local_parse(page: String) -> Option<String> {
         .next()
         .and_then(|e| e.value().attr("href"))
         .map(|s| s.to_string())
+}
+
+async fn update_kok_catalogue(ctx: &Context, name: String, user: &User) {
+    let channel_id = if let Ok(channel) = get_channel_id("suppekjøkkenet", &ctx.http).await {
+        channel
+    } else {
+        tracing::warn!("Not able to get channel id");
+        return;
+    };
+
+    let mut bot_messages = Vec::with_capacity(100);
+    let mut messages = channel_id.messages_iter(&ctx.http).boxed();
+    while let Some(message) = messages.next().await {
+        let message = match message {
+            Ok(message) => message,
+            Err(why) => {
+                tracing::warn!("Not able to get message: {:?}", why);
+                continue;
+            }
+        };
+        if message.author.id == ctx.cache.current_user_id() {
+            bot_messages.push(message);
+        }
+    }
+    bot_messages = bot_messages
+        .into_iter()
+        .filter_map(|m| {
+            if let Some(embed) = m.embeds.get(0) {
+                if embed.title.as_ref().unwrap_or(&"".to_string()) == &name {
+                    Some(m)
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        })
+        .collect();
+    // Find first
+    let message_id = bot_messages.get(0).map(|m| m.id);
+    let mut updated = false;
+    if let Some(message_id) = message_id {
+        match channel_id.delete_message(&ctx.http, message_id).await {
+            Ok(_) => updated = true,
+            Err(why) => tracing::warn!("Not able to delete message: {:?}", why),
+        }
+    }
+
+    let guild_id = GuildId(
+        env::var("GUILD_ID")
+            .expect("Expected GUILD_ID in environment")
+            .parse()
+            .expect("GUILD_ID must be an integer"),
+    ); // Maybe store guildID in the cache?
+
+    let user_name = user
+        .nick_in(&ctx.http, guild_id)
+        .await
+        .unwrap_or_else(|| user.name.clone());
+
+    if let Err(why) = channel_id
+        .send_message(&ctx.http, |m| {
+            m.embed(|e| {
+                e.title(name)
+                    .description(if updated {
+                        format!("{} has updated the file", user_name)
+                    } else {
+                        format!("{} has added a new file", user_name)
+                    })
+                    .color(0x00ff00)
+            })
+        })
+        .await
+    {
+        tracing::warn!("Not able to send message: {:?}", why);
+    }
 }
 
 // Interaction handling to this Modal is handled in lib.rs file in the interaction_create function
